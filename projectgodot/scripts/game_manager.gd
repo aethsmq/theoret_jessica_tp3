567248978722ffb6ef2689f0138c55cd64a6a30f
Jess <2396043@cmontmorency.qc.ca>
2025-11-20 23:40:46 -0500

    commit15

--- projectgodot/scripts/game_manager.gd
+++ projectgodot/scripts/game_manager.gd
@@ -3,10 +3,8 @@ extends Node
 var score = 0
 @onready var score_label: Label = $ScoreLabel
 
-
 func add_point():
 	score += 1
 	print(score)
-	score_label.text = "you collected " + str(score) + "/16 !!"
-	
+	score_label.text = "" + str(score) + "/16"
 	
